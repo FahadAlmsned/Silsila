294d58bb1c79a96e166d315b9f4eba2775bf8897
Fahad Moqbel Almsned <33640358+FahadAlmsned@users.noreply.github.com>
2026-09-14 08:44:06 +0300

    تصحيح site و base في astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// Update `site` and `base` to match your GitHub Pages URL.
-// If your repo is USERNAME.github.io -> base: '/'
-// If your repo is USERNAME.github.io/REPO_NAME -> base: '/REPO_NAME/'
 export default defineConfig({
-  site: 'https://YOUR_USERNAME.github.io',
-  base: '/REPO_NAME/',
+  site: 'https://FahadAlmsned.github.io',
+  base: '/Silsila/',
 });
